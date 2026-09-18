d4a73d37f6d476f3bb9c05047efad8ed110add87
rollnroll <calendarchat@proton.me>
2026-09-18 04:51:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -51,13 +51,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
-});const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+});
 
 // Раздаем статические файлы (ваш HTML, CSS и JS) из папки 'public'
 app.use(express.static('public'));
