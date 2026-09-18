59f3e54639d5facfd475261c97c3233788d8d7fe
rollnroll <calendarchat@proton.me>
2026-09-18 04:57:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,26 +52,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
-
-// Раздаем статические файлы (ваш HTML, CSS и JS) из папки 'public'
-app.use(express.static('public'));
-
-// Обработка подключения игроков
-io.on('connection', (socket) => {
-    console.log(`Игрок подключился: ${socket.id}`);
-
-    // Когда игрок делает ход
-    socket.on('roll_dice', (data) => {
-        // Пересылаем ход сопернику
-        socket.broadcast.emit('opponent_rolled', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log(`Игрок отключился: ${socket.id}`);
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
